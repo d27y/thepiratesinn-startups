6b7ac1e2bfc265c8021343661300e4c4165aa530
Timo Derstappen <teemow@gmail.com>
2013-07-24 07:48:27 +0000

    fixed url rewrite for html5mode on heroku too

--- heroku/server.js
+++ heroku/server.js
@@ -1,5 +1,24 @@
 'use strict';
 
 var statik = require('statik');
-var server = statik.createServer('.');
-server.listen();
+var connect = require('connect');
+var app = statik({
+  port: process.env.PORT,
+  root: __dirname
+});
+
+// hackish mod_rewrite for html5mode
+app.use(function(req, res, next) {
+  req.url = "/";
+  next();
+});
+var compress = connect.compress();
+var statik = connect.static(__dirname, {
+  port: process.env.PORT,
+  maxAge: 0,
+  hidden: false,
+  redirect: true,
+  compress: true
+});
+app.use(compress);
+app.use(statik);
